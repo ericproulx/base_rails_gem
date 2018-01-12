a30c4773b02bea17a9fe4edc2c2158cc121340cb
Eric Proulx <eproulx@petalmd.com>
2018-01-11 22:13:39 -0500

    Added other test_app versions

---
+++ spec/test_app_4_0/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp40::Application.config.secret_key_base = '8bed1ad3cfc34f433d914c9842925366ddecf6767864e369a2bfab2f3633659d2e2e4be96e4684d9a4dd4aea448e9cf4b7883054e791a037477bd94b22289932'
---
+++ spec/test_app_4_0/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestApp40::Application.config.session_store :cookie_store, key: '_test_app_4_0_session'
